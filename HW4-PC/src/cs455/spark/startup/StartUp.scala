d6d9f414943b903eea47cc50ca4969739fa01581
chiranjeb <38108328+chiranjeb@users.noreply.github.com>
2018-04-08 04:00:17 -0700

    Adding first spark standalone program
    -Added Maven Script to build the spark  program written in scala
    -A Basic word count program and intial directory structure

---
+++ HW4-PC/src/cs455/spark/startup/StartUp.scala
@@ -0,0 +1,12 @@
+package cs455.spark.startup
+import cs455.spark.wordcount.WordCount
+
+//Main program entry.
+object StartUp 
+{
+  def main(args: Array[String]): Unit = 
+  {
+     println("Word Count program");
+     new WordCount().Execute(args(0), args(1));
+  }
+}
