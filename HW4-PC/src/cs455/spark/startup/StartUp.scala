f20904b1f00f67d2f3e0c87a4b9092eaf4396eb0
chiranjeb <38108328+chiranjeb@users.noreply.github.com>
2018-04-17 12:45:55 -0700

    Add files via upload

--- HW4-PC/src/cs455/spark/startup/StartUp.scala
+++ HW4-PC/src/cs455/spark/startup/StartUp.scala
@@ -1,12 +1,18 @@
 package cs455.spark.startup
 import cs455.spark.wordcount.WordCount
+import cs455.spark.employment.TotalEmploymentAnalyzer
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
 
 //Main program entry.
 object StartUp 
 {
   def main(args: Array[String]): Unit = 
   {
-     println("Word Count program");
-     new WordCount().Execute(args(0), args(1),args(2));
+     val conf = new SparkConf().setAppName("hw4-assignment").setMaster(args(0));
+     val sc = new SparkContext(conf);
+
+     //println("Word Count program");
+     new TotalEmploymentAnalyzer().Execute(sc, (args(1) + "/employment_data"), args(2));
   }
 }
